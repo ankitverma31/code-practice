6b9f0000788d2d95c88b7aaaadaa763f945ae32e
Ankit Verma <ankit.v@media.net>
2022-03-05 15:09:16 +0530

    duplicate finder

---
+++ findDuplicate.js
@@ -0,0 +1,17 @@
+// Problem Statement: Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+// areThereDuplicates(1, 2, 3) // false
+// areThereDuplicates('a', 'b', 'c', 'a') // true
+// areThereDuplicates(1, 2, 2) // true
+
+function areThereDuplicates(...args) {
+  let collection = {};
+  for (let val in args) {
+    collection[arguments[val]] = (collection[arguments[val]] || 0) + 1;
+  }
+  for (let key in collection) {
+    if (collection[key] > 1) return true;
+  }
+  return false;
+}
+
+console.log(areThereDuplicates(1, 2, 2));
