8cd0df187606b26055ab6fb60ade02c70fe04f47
Ankit Verma <ankit.v@media.net>
2022-03-05 19:04:20 +0530

    average pairs code

--- averagePairs.js
+++ averagePairs.js
@@ -6,25 +6,25 @@
 // averagePair([],4) // false
 
 function averagePair(arr, k) {
-    if(arr.length == 0) {
-        return false;
-    }
-    let left = 0;
-    let right = arr.length - 1;
-    while(left <  right) {
-        let sum = arr[left]+arr[right];
-        if(sum/2 == k){
-            return true;
-        } else if(sum/2 > k) {
-            right--;
-        } else {
-            left++
-        }
-    }
+  if (arr.length == 0) {
     return false;
+  }
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum / 2 == k) {
+      return true;
+    } else if (sum / 2 > k) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+  return false;
 }
 
-console.log(averagePair([1,3,3,5,6,7,2,12,19],8));
-console.log(averagePair([1,2,3],2.5));
-console.log(averagePair([-1,0,3,4,5,6], 4.1));
-console.log(averagePair([],4));
+console.log(averagePair([1, 3, 3, 5, 6, 7, 2, 12, 19], 8));
+console.log(averagePair([1, 2, 3], 2.5));
+console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1));
+console.log(averagePair([], 4));
