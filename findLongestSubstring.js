0d8989eff211750e8e16c6fd170c302b7fdf2291
Ankit Verma <ankit.v@media.net>
2022-03-06 18:33:53 +0530

    min subarray

---
+++ findLongestSubstring.js
@@ -0,0 +1,14 @@
+// Problem Statement: Write a function called findLongestSubstring, which accepts a string and returns the length of the longest substring with all distinct characters.
+// Time Complexity - O(n)
+
+// findLongestSubstring('') // 0
+// findLongestSubstring('rithmschool') // 7
+// findLongestSubstring('thisisawesome') // 6
+// findLongestSubstring('thecatinthehat') // 7
+// findLongestSubstring('bbbbbb') // 1
+// findLongestSubstring('longestsubstring') // 8
+// findLongestSubstring('thisishowwedoit') // 6
+
+function findLongestSubstring(str) {
+    
+}
