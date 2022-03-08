5369579d1ac96b9253d3585541d00305d30f6ca5
Ankit Verma <ankit.v@media.net>
2022-03-08 12:42:33 +0530

    longest substring

--- findLongestSubstring.js
+++ findLongestSubstring.js
@@ -10,5 +10,21 @@
 // findLongestSubstring('thisishowwedoit') // 6
 
 function findLongestSubstring(str) {
-    
+  let start = 0;
+  let maxLen = 0;
+  let map = {};
+  if (str.length === 0) {
+    return 0;
+  }
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (map[char]) {
+      start = Math.max(start, map[char]);
+    }
+    map[char] = i + 1;
+    maxLen = Math.max(maxLen, i - start + 1);
+  }
+  return maxLen;
 }
+
+console.log(findLongestSubstring("rithmschool"));
