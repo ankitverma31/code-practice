c5aad0a94a8c8132ba9207bbc022cef7a0aaf07a
Ankit Verma <ankit.v@media.net>
2022-06-07 09:55:34 +0530

    currying js

---
+++ shortcodes/currying.js
@@ -0,0 +1,17 @@
+// Problem Statement: Advanced Currying
+
+const curry = (fn) => {
+  return (curried = (...args) => {
+    if (fn.length !== args.length) {
+      return curried.bind(null, ...args);
+    }
+    return fn(...args);
+  });
+};
+
+const totalNum = (a, b, c) => a + b + c;
+
+const curriedTotal = curry(totalNum);
+console.log(curriedTotal(10)(20)(30));
+console.log(curriedTotal(10, 20, 30));
+console.log(curriedTotal(10, 20)(30));
